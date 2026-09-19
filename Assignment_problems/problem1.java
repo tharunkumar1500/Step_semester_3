89c2122c73c26745dd221b51d81789ca85d3ce99
BL-Tharunkumar <tharunkumar.lakshmanan@bridgelabz.com>
2026-09-19 16:37:29 +0530

    week 1 is completed

--- Assignment_problems/problem1.java
+++ Assignment_problems/problem1.java
@@ -1,7 +1,8 @@
 
-
-public class problem1{
+public class problem1 {
     public static void main(String[] args) {
-        
+
+        System.out.println("Assigments problems");
+
     }
 }
--- practice_problem/problem1.java
+++ practice_problem/problem1.java
@@ -3,5 +3,7 @@ package practice_problem;
 public class problem1 {
     public static void main(String[] args) {
 
+        System.out.println("");
+
     }
 }
